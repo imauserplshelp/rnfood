3dd44b5194279669f91d6168148dbdd5e849e3a5
user0 <user0@user0.local>
2020-05-28 22:38:45 -0400

    Sec 8 -- Assembling a Navigator

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const SearchScreen = () => {
+  return <View>
+    <Text>Search Screen</Text>
+   </View>
+};
+
+const styles = Stylesheet.create({});
+
+export default SearchScreen;
