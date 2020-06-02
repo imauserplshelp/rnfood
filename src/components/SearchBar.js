2487dd56aa74e6ca2a4ea37ba684e1443a513c30
user0 <user0@user0.local>
2020-06-02 19:28:11 -0400

    Sec 8 -- Architecture Approach -- Created components dir and SearchBar.js w/ boilerplate

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  	);
+  };
+
+
+const styles = Stylesheet.create({});
+
+export default SearchBar;
+
+
+
