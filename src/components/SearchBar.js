decd992391a5a3217e97b36218d35920946c2a89
user0 <user0@user0.local>
2020-06-03 04:19:01 -0400

    Sec 8 -- Starting the SearchBar AND Displaying Icons -- styled SearchBar background and imported and styled search icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Icon, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
+      <Feather name="search" size={30} />
       <Text>Search Bar</Text>
     </View>
-  	);
-  };
+  );
+ };
 
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#F0EEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15 
+	}
+});
 
 export default SearchBar;
 
